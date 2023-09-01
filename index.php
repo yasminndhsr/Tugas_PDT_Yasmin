794235f7f669a648d9cf128bf46c3f0b43c918ef
Yasmin Indah Hasari <106302985+yasminndhsr@users.noreply.github.com>
2023-09-01 23:14:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas PDT Yasmin</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: none;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+        }
+        h1 {
+            margin: 0;
+        }
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Profil Pribadi</h1>
+    </header>
+    <div class="container">
+        <?php
+        echo "<p>Nama : Yasmin Indah Hasari</p>";
+        echo "<p>>NPM : 217006009</p>";
+        echo "<p>Universitas Siliwangi</p>";
+        ?>
+    </div>
+</body>
+</html>
